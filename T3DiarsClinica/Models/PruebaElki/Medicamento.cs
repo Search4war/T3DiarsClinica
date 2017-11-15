99d5cf2c9dda03676467389020d4e8966395792c
ElkiDIARS <elkisoft.s3@gmail.com>
2017-11-15 11:34:44 -0500

    Medci

---
+++ T3DiarsClinica/Models/PruebaElki/Medicamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T3DiarsClinica.Models.PruebaElki
+{
+    public class Medicamento
+    {
+        public int Id { get; set; }
+
+    }
+}
