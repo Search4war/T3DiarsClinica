28bf8d4ad66daee0c88392760525add376f02942
ElkiDIARS <elkisoft.s3@gmail.com>
2017-11-15 11:43:39 -0500

    Lab

---
+++ T3DiarsClinica/Models/PruebaElki/Laboratorio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T3DiarsClinica.Models.PruebaElki
+{
+    public class Laboratorio
+    {
+        public int Id { get; set; }
+    }
+}
