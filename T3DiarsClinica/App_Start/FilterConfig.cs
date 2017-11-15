bc300d1e75335a14dc05a016703052367d442bea
Search4war <sergiomore29@hotmail.com>
2017-11-15 11:06:45 -0500

    proyecto

---
+++ T3DiarsClinica/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace T3DiarsClinica
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ T3DiarsClinica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(T3DiarsClinica.Startup))]
+namespace T3DiarsClinica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
